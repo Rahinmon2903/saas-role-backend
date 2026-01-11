490aa360275978e9d249b7269754faa5b3eb1cde
Rahinmon2903 <rahin2903@gmail.com>
2026-01-11 11:07:56 +0530

    request Schema added

---
+++ Model/requestSchema.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const requestSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    assignedTo: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true, // manager
+    },
+    remark: {
+      type: String,
+    },
+  },
+  { timestamps: true }
+);
+
+const Request = mongoose.model("Request", requestSchema);
+export default Request;
