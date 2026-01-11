52e87167200d991baff4c77b0464535ed4e4f351
Rahinmon2903 <rahin2903@gmail.com>
2026-01-11 12:10:10 +0530

    role updating only by admin completed

--- Controller/adminController.js
+++ Controller/adminController.js
@@ -12,3 +12,35 @@ export const getAllUsers = async (req, res) => {
     res.status(500).json({ message: "Failed to fetch users" });
   }
 };
+
+//update role only admin
+
+export const updateUserRole = async (req, res) => {
+  try {
+    const {  role } = req.body;
+    const userId = req.params.id;
+    if(!["admin", "manager", "user"].includes(role)){
+      return res.status(400).json({ message: "Invalid role" });
+    }
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    if(req.user._id === userId){
+      return res.status(403).json({ message: "Cannot update your own role" });
+    }
+    user.role = role;
+    await user.save();
+      res.json({
+      message: "Role updated successfully",
+      user: {
+        id: user._id,
+        name: user.name,
+        role: user.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Failed to update user role" });
+  }
+};
