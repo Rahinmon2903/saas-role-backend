a0c45e6e2af79f9e99995f41023a66b5c3f68456
Rahinmon2903 <rahin2903@gmail.com>
2026-01-16 10:11:19 +0530

    userSchema updated

--- Model/userSchema.js
+++ Model/userSchema.js
@@ -2,28 +2,34 @@ import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
+    // username
     name: {
       type: String,
       required: true
     },
+    //email
     email: {
       type: String,
       required: true,
       unique: true
     },
+    //password
     password: {
       type: String,
       required: true
     },
+    //in our system we have three roles the default one is user
     role: {
       type: String,
       enum: ["admin", "manager", "user"],
       default: "user"
     },
+    //used for password reset
     resetToken: {
   type: String,
   default: null
 },
+//expire time
 resetTokenExpire: {
   type: Date,
   default: null
